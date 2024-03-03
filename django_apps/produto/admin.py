2b0c0505791facacb78c4adb7ec8ca599dd40c6e
Alixandria007 <juninhokrauts778@gmail.com>
2024-03-03 20:31:06 -0300

    Adicionei a Tabular inline ao model Produto

--- django_apps/produto/admin.py
+++ django_apps/produto/admin.py
@@ -2,12 +2,20 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+
+class VariacaoInline(admin.TabularInline):
+    model = models.Variacao
+    extra = 1
+
 @admin.register(models.Produto)
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = 'nome', 'preco_marketing', 'preco_marketing_promocional' , 'tipo'
     prepopulated_fields = {
         'slug' : ('nome',)
     }
+    inlines = VariacaoInline ,
+
+
 
 @admin.register(models.Variacao)
 class VariacaoAdmin(admin.ModelAdmin):
