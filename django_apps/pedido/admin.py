04013df9a43fdf082c151396681641926f09d3dc
Alixandria007 <juninhokrauts778@gmail.com>
2024-03-04 16:31:18 -0300

    Adicionei o model Pedido ao projeto

--- django_apps/pedido/admin.py
+++ django_apps/pedido/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
 
+@admin.register(models.Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = 'cliente' , 'total' ,'status'
--- django_apps/pedido/models.py
+++ django_apps/pedido/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Pedido(models.Model):
+    class Meta:
+        verbose_name = 'Pedido'
+        verbose_name_plural = 'Pedidos'
+
+    cliente = models.ForeignKey(User, on_delete = models.CASCADE)
+    total = models.FloatField(default = 0)
+    status = models.CharField(max_length = 1 ,choices = (('A' ,'Aprovado'),
+                                          ('R' , 'Reprovado'),
+                                          ('C', 'Criado'),
+                                          ('P', 'Pendente'),
+                                          ('E' , 'Enviado'),
+                                          ('F' , 'Finalizado')
+                                          ))
+    
+    def __str__(self):
+        return f'Pedido de {self.cliente}'
