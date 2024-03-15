f63305fbb4a7c99240ced09ed83e01f4d581403c
Alixandria007 <juninhokrauts778@gmail.com>
2024-03-15 19:54:31 -0300

    Corrigindo alguns erros

--- django_apps/produto/urls.py
+++ django_apps/produto/urls.py
@@ -5,8 +5,8 @@ app_name = 'produto'
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('<slug:slug>/',views.detalhes, name='detalhes'),
-    path('adicionar_carrinho/',views.index, name='adicionar_carrinho'),
+    path('produto/<slug:slug>/',views.detalhes, name='detalhes'),
+    path('adicionar_carrinho/',views.adicionar_carrinho, name='adicionar_carrinho'),
     path('remover_carrinho/',views.index, name='remover_carrinho'),
     path('carrinho/',views.index, name='carrinho'),
     path('finalizar/',views.index, name='finalizar'),
--- django_apps/produto/views.py
+++ django_apps/produto/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.core.paginator import Paginator
+from django.contrib import messages
 from . import models
 # Create your views here.
 
@@ -21,7 +23,40 @@ def detalhes(request, slug):
     produto = models.Produto.objects.filter(slug = slug).first()
 
     context = {
-        'produto' : produto
+        'prod' : produto
+    }
+
+    print('Jambra')
+    return render(request,'loja/pages/detail.html', context)
+
+
+def adicionar_carrinho(request):
+    
+    produto = models.Produto.objects.all().first()
+    messages.error(
+        request,
+        'Erro'
+    )
+
+    
+    return redirect('produto:index')
+
+
+    
+
+def remover_carrinho(request):
+
+    context = {
+        'produto' : 1
+    }
+
+    return render(request,'loja/pages/detail.html', context)
+
+
+def carrinho(request):
+
+    context = {
+        'produto' : 1
     }
 
     return render(request,'loja/pages/detail.html', context)
